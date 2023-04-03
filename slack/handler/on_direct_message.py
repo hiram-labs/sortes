8c585c8ffc4d5c767cd2544b45bdd9b0d5b9cdb6
Owusu <adjeibohyen@hotmail.co.uk>
2023-04-03 16:58:23 +0100

    ref: make handlers async

--- slack/handler/on_direct_message.py
+++ slack/handler/on_direct_message.py
@@ -6,10 +6,10 @@ from typing import Callable
 logger = logging.getLogger(__name__)
 
 
-def event(body, say, agent: Callable):
+async def event(body, say, agent: Callable):
     bot_user_id = reduce(operator.getitem, ["authorizations", 0, "user_id"], body)
     event = reduce(operator.getitem, ["event"], body)
     message = event.get("text")
-    agent_response = agent(message.replace(f"<@{bot_user_id}>", "").strip())
+    agent_response = await agent(message.replace(f"<@{bot_user_id}>", "").strip())
     agent_response = f"{agent_response}"
-    return say(text=agent_response)
+    return await say(text=agent_response)
--- slack/handler/on_mention.py
+++ slack/handler/on_mention.py
@@ -6,12 +6,12 @@ from typing import Callable
 logger = logging.getLogger(__name__)
 
 
-def event(body, say, agent=Callable):
+async def event(body, say, agent=Callable):
     bot_user_id = reduce(operator.getitem, ["authorizations", 0, "user_id"], body)
     event = reduce(operator.getitem, ["event"], body)
     thread_ts = event.get("thread_ts") or event.get("ts")
     sender_user_id = event.get("user")
     message = event.get("text")
-    agent_response = agent(message.replace(f"<@{bot_user_id}>", "").strip())
+    agent_response = await agent(message.replace(f"<@{bot_user_id}>", "").strip())
     agent_response = f"<@{sender_user_id}> {agent_response}"
-    return say(text=agent_response, thread_ts=thread_ts)
+    return await say(text=agent_response, thread_ts=thread_ts)
